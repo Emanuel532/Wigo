d918ee6625f9cedb79987f3ee976e213c77138aa
Emanuel532 <ungureanuemanuelconstantin@yahoo.com>
2024-05-15 20:07:36 +0300

    screen overflow fix for sign_up & log_in pages

--- lib/screens/log_in_screen.dart
+++ lib/screens/log_in_screen.dart
@@ -18,137 +18,143 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WIGO',
-              style: GoogleFonts.raleway(
-                fontWeight:FontWeight.w800,
-                fontSize: 100,
-                color: Color.fromARGB(255, 85, 157, 199)
-              ),
+        title: Text(
+          'WIGO',
+          style: GoogleFonts.raleway(
+            fontWeight: FontWeight.w800,
+            fontSize: 100,
+            color: Color.fromARGB(255, 85, 157, 199),
+          ),
         ),
         toolbarHeight: 90,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Text('Sign in',
-                  style: GoogleFonts.raleway(
-                    fontWeight:FontWeight.w600,
-                    fontSize: 64,
-                    color: Color.fromARGB(255, 85, 157, 199),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 2.0),
-            EmailInput(emailController: emailController,),
-            SizedBox(height: 16.0),
-            PasswordInput(passwordController: passwordController),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                
-                TextButton(
-                  onPressed: (){},
-                  style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                    (Set<MaterialState> states) {
-                      // Disables the hover animation
-                      if (states.contains(MaterialState.hovered)) {
-                        return Colors.transparent;
-                      }
-                      return null; // Use the default color
-                    },
-                  ),
-                ),
-                  child: Text('Forgot your password?',
-                                style: GoogleFonts.raleway(
-                  fontWeight:FontWeight.w400,
-                  fontSize: 16,
-                  color: Color.fromARGB(255, 85, 157, 199)
-                                ),
-                          ),
-                ),
-              ],
-            ),
-            SizedBox(height: 32.0),
-            //todo: ADD A loading progress indicator
-            Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              // Sign in or login
-              if (!emailController.text.isEmpty &&
-                  !passwordController.text.isEmpty) {
-                AccountController(AuthenticationService())
-                    .signIn(
-                      email: emailController.text,
-                      password: passwordController.text,
-                    )
-                    .then((_) {
-                  if (AuthenticationUtils.isUserAuthenticated()) {
-                    GoRouter.of(context).pushReplacement('/');
-                  }
-                }).catchError((err) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(err.toString()),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Sign in',
+                    style: GoogleFonts.raleway(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 64,
+                      color: Color.fromARGB(255, 85, 157, 199),
                     ),
-                  );
-                });
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 85, 157, 199),
-              foregroundColor: Colors.white,
-              elevation: 3,
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
+                  ),
+                ],
               ),
-            ),
-            child: Text(
-              'Log in',
-              style: GoogleFonts.raleway(
-                fontWeight: FontWeight.w400,
-                fontSize: 32,
-                color: Color.fromARGB(255, 255, 255, 255),
+              SizedBox(height: 2.0),
+              EmailInput(
+                emailController: emailController,
               ),
-            ),
+              SizedBox(height: 16.0),
+              PasswordInput(
+                passwordController: passwordController,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                          // Disables the hover animation
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.transparent;
+                          }
+                          return null; // Use the default color
+                        },
+                      ),
+                    ),
+                    child: Text(
+                      'Forgot your password?',
+                      style: GoogleFonts.raleway(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 85, 157, 199),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 32.0),
+              //todo: ADD A loading progress indicator
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Sign in or login
+                      if (!emailController.text.isEmpty &&
+                          !passwordController.text.isEmpty) {
+                        AccountController(AuthenticationService())
+                            .signIn(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        )
+                            .then((_) {
+                          if (AuthenticationUtils.isUserAuthenticated()) {
+                            GoRouter.of(context).pushReplacement('/');
+                          }
+                        }).catchError((err) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(err.toString()),
+                            ),
+                          );
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 85, 157, 199),
+                      foregroundColor: Colors.white,
+                      elevation: 3,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    child: Text(
+                      'Log in',
+                      style: GoogleFonts.raleway(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 32,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'or',
+                    style: GoogleFonts.raleway(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 32,
+                      color: Color.fromARGB(255, 85, 157, 199),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  GoogleSignInButton(
+                    contextt: context,
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.0),
+              TextButton(
+                onPressed: () {
+                  // goRoute that takes the user to the new account creation screen
+                  GoRouter.of(context).go('/login/create_account');
+                },
+                child: Text('Create New Account'),
+              ),
+              SizedBox(height: 8.0),
+            ],
           ),
-          SizedBox(height: 10),
-          Text(
-            'or',
-            style: GoogleFonts.raleway(
-              fontWeight: FontWeight.w400,
-              fontSize: 32,
-              color: Color.fromARGB(255, 85, 157, 199),
-            ),
-          ),
-          SizedBox(height: 10),
-          GoogleSignInButton(
-            contextt: context,
-          ),
-        ],
-      ),
-
-            SizedBox(height: 8.0),
-            TextButton(
-              onPressed: () {
-                // goRoute that takes the user to the new account creation screen
-                GoRouter.of(context).go('/login/create_account');
-              },
-              child: Text('Create New Account'),
-            ),
-            SizedBox(height: 8.0),
-
-          
-          ],
         ),
       ),
     );
