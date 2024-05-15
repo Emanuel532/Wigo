4f4f605180982aed8d757015988f38ad12d65049
Mario Mairovitz <62700212+mairovitzmario@users.noreply.github.com>
2024-05-15 22:18:09 +0300

    edited carousel

--- lib/widgets/itinerary.dart
+++ lib/widgets/itinerary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ItineraryBox extends StatefulWidget {
   final int days;
@@ -11,23 +12,75 @@ class ItineraryBox extends StatefulWidget {
 }
 
 class _ItineraryBoxState extends State<ItineraryBox> {
+  final CarouselController _controller = CarouselController();
+
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: widget.days,
-      options: CarouselOptions(
-        viewportFraction: 1.0,
-        enableInfiniteScroll: false,
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 85, 157, 199),
+        borderRadius: BorderRadius.circular(10.0),
       ),
-      itemBuilder: (BuildContext context, int index, int realIndex) {
-        int dayNumber = index + 1;
-        return ListTile(
-          title: Text('Day $dayNumber'),
-          subtitle: SingleChildScrollView(
-            child: Text('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n\n\n'),
-          ), // Replace with your content
-        );
-      },
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CarouselSlider.builder(
+            carouselController: _controller,
+            itemCount: widget.days,
+            options: CarouselOptions(
+              viewportFraction: 1.0,
+              enableInfiniteScroll: true,
+            ),
+            itemBuilder: (BuildContext context, int index, int realIndex) {
+              int dayNumber = index + 1;
+              return ListTile(
+                title: Text(
+                  'Day $dayNumber',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 42,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+                subtitle: SingleChildScrollView(
+                  child: Text(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n\n\n'),
+                ), // Replace with your content
+              );
+            },
+          ),
+          SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  _controller.previousPage();
+                },
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  _controller.nextPage();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
